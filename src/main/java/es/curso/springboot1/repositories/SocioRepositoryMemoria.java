745da26853a4a87b1f9c5fabb5d2b18496a279ba
Torre68 <alditorredero@gmail.com>
2025-02-11 11:38:19 +0100

    ADT 11/02/2025

--- src/main/java/es/curso/springboot1/repositories/SocioRepositoryMemoria.java
+++ src/main/java/es/curso/springboot1/repositories/SocioRepositoryMemoria.java
@@ -5,11 +5,13 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import es.curso.springboot1.negocio.Socio;
 @Repository
-public class SocioRepositoryMemoria {
+@Qualifier("memoria")
+public class SocioRepositoryMemoria implements SocioRepository {
 
     List<Socio> socios = new ArrayList<>();
     
@@ -21,6 +23,7 @@ public class SocioRepositoryMemoria {
         socios.add(new Socio("Alde", "Zhang", 45));
     }
     
+    @Override
     public void borrarSocio(String nombre) {
         Socio s = new Socio(nombre);
         socios.remove(s);
@@ -28,20 +31,24 @@ public class SocioRepositoryMemoria {
     }
 
     
+    @Override
     public void insertarSocio(Socio socio) {
         socios.add(socio);
 
     }
 
+    @Override
     public List<Socio> buscarTodos(){
 
         return socios;
     }
 
+    @Override
     public Optional<Socio> buscarUno (String nombre){
         return socios.stream().filter((s) -> s.getNombre().equals(nombre)).findFirst();
     }
 
+    @Override
     public List<Socio> buscarTodosOrdenados (String orden){
 
         List<Socio> listaOrdenada = new ArrayList<>();
