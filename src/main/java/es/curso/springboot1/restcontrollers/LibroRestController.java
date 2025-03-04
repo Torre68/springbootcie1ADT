9f7d7c66c19c42a37312012556bc95386589f41b
Torre68 <alditorredero@gmail.com>
2025-03-04 13:56:07 +0100

    04/03/2025

---
+++ src/main/java/es/curso/springboot1/restcontrollers/LibroRestController.java
@@ -0,0 +1,31 @@
+package es.curso.springboot1.restcontrollers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.curso.springboot1.negocio.Libro;
+import es.curso.springboot1.repositories.LibroRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/webapi/libros")
+public class LibroRestController {
+
+@Autowired 
+@Qualifier("jdbc")
+
+    private LibroRepository libroRepository;
+
+    @GetMapping
+   
+    public List<Libro> buscarTodos (){
+
+        return libroRepository.buscarTodos();
+    }
+
+}
--- src/main/java/es/curso/springboot1/restcontrollers/SocioRestController.java
+++ src/main/java/es/curso/springboot1/restcontrollers/SocioRestController.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import es.curso.springboot1.negocio.Socio;
 import es.curso.springboot1.repositories.SocioRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -33,6 +33,7 @@ public class SocioRestController {
 
         return socioRepository.buscarTodos();
     }
+    
 
 }
 
